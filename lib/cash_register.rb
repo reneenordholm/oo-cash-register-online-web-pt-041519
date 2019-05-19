776c2a165f9daf6d3e20682deb297dc5df70c08e
Renee Nordholm <reneenordholm@gmail.com>
2019-05-19 21:42:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
   
   def initialize(discount = 0, total = 0)
     @total = total
@@ -20,8 +20,5 @@ class CashRegister
       return "After the discount, the total comes to $#{@total}."
     end
   end 
-  
-  def items
-    @items << self.items
-  end
+
 end
